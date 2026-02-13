ddaaf2bab5b392877e8871e9b80aaff3e004e069
clemlery <lerouleyclement@gmail.com>
2026-02-13 22:39:40 +0100

    Add 2 methods in CsvDAO.kt : searchPatterns and loadAllPatterns.

--- src/main/kotlin/gameoflifetdd/model/dataccess/CsvDAO.kt
+++ src/main/kotlin/gameoflifetdd/model/dataccess/CsvDAO.kt
@@ -4,19 +4,26 @@ import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 import gameoflifetdd.config.ModelConfig
 import java.io.File
+import kotlin.text.uppercase
 
-object CsvDAO {
-    fun load(
-        file: File,
-        limit: Int
-    ): List<Pattern> {
+class CsvDAO(
+    source: File,
+    val bookmarks: File
+) {
+
+    private val patterns : MutableList<Pattern> = mutableListOf()
+
+    init {
+        loadAllPatterns(source)
+    }
+
+    fun loadAllPatterns(file: File) {
         val lines = csvReader {
             delimiter = ','
             quoteChar = '"'
             charset = Charsets.UTF_8.toString()
         }.readAllWithHeader(file).drop(1)
 
-        val patterns = mutableListOf<Pattern>()
         lines.forEach { line ->
             try {
                 patterns.add(Pattern(
@@ -27,16 +34,37 @@ object CsvDAO {
                 return@forEach
             }
         }
+    }
+
+    fun searchPatterns(
+        search: String = "",
+        limit: Int = 9,
+        offset: Int = 0
+    ): MutableList<Pattern> {
+        val patternsFound = mutableListOf<Pattern>()
+        val patternsUsed = if (search != "") {
+            patterns.filter { it.toString().contains(search) }
+        } else {
+            patterns
+        }
+        if (patternsUsed.isEmpty()) return mutableListOf()
+        val stop = if (offset+limit > patternsUsed.size) {
+            patternsUsed.size
+        } else {
+            offset+limit
+        }
+        for (i in offset+1 until stop) {
+            patternsFound.add(patternsUsed[i])
+        }
         return patterns
     }
 
-    fun write(
-        file: File,
+    fun addToBookmarks(
         pattern: Pattern
     ) {
         csvWriter {
             delimiter = ','
-        }.open(file, append = true) {
+        }.open(bookmarks, append = true) {
             writeRow(pattern.name, pattern.type.name.lowercase())
         }
     }
